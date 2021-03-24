b87e73469fa58055e2e55ef0c5aeb449391ed959
Santiago Torres <torres_santiago@javeriana.edu.co>
2021-03-24 11:56:56 +0100

    added testGPSConnection() and setNewGPSRate() functions

--- Sources/gps.cpp
+++ Sources/gps.cpp
@@ -15,15 +15,51 @@
 
 using namespace std;
 
+
+/**
+ * \brief Test the sensor is connected and can communicate with the CPU
+ * \param[out] connectiontest returns 1 if sensor didn't respond the request, 0 otherwise
+ * \param[in] gps The gps ublox object
+ */
+bool testGPSConnection(Ublox gps){
+    // testConnection() returns 0 if it can establish an SPI connection with the
+    // sensor. If somehow it can't, a 1 will be returned. If in the first 300 symbols a
+    // a message is correct, the test is passed.
+    if(gps.testConnection()){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+/**
+ * \brief Set a data capture rate easily
+ * \param[out] GPSRateAssert returns 0 if the rate couldn't be configured correctly
+ * \param[in] gps The gps ublox object
+ * \param[in] rate The data rate in ... [missing unit]
+ */
+bool setNewGPSRate(gps, int rate){
+    // ConfigureSolutionRate() allows the user to set a fixed data capture rate
+    if (!gps.configureSolutionRate(rate)){
+        return 0;
+    }
+}
+
 int main(int argc, char *argv[]){
 
 
     // We create a dynamic vector to save our position data.
     std::vector<double> pos_data;
 
-    // create ublox
+    // The navio's  GPS uses ublox protocol for SPI communication. This is
+    // a propietary protocol by Ublox that allows perfect clock synchronisation.
     Ublox gps;
 
+    // We test the connection to the sensor    
+    testGPSConnection(gps);
+
+    // We set the new rate for data capture
+    setNewGPSRate(gps, 1000);
 
     return 0;
 }
