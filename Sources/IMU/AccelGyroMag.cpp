0e8326c36c568a324d5eb52ff69a639b313a51d9
Maria Yammine <56266574+Maria-yammine@users.noreply.github.com>
2021-04-02 12:05:09 +0200

    Update AccelGyroMag.cpp

--- Sources/IMU/AccelGyroMag.cpp
+++ Sources/IMU/AccelGyroMag.cpp
@@ -23,16 +23,16 @@
  * \brief This function allows you to choose which of the two sensors : 
  * MPU or LSM you wish to use
  * \param[out] ptr the pointer to the chosen measurement unit
- * \param[in] name the name of the sensor you want  
+ * \param[in] sensor_name the name of the sensor you choose  
  */
-std::unique_ptr <InertialSensor> getSensor( std::string name)
+std::unique_ptr <InertialSensor> get_inertial_sensor( std::string sensor_name)
 {
-    if (name == "mpu") {
+    if (sensor_name == "mpu") {
         printf("Selected: MPU9250\n");
         auto ptr = std::unique_ptr <InertialSensor>{ new MPU9250() };
         return ptr;
     }
-    else if (name == "lsm") {
+    else if (sensor_name == "lsm") {
         printf("Selected: LSM9DS1\n");
         auto ptr = std::unique_ptr <InertialSensor>{ new LSM9DS1() };
         return ptr;
@@ -41,3 +41,88 @@ std::unique_ptr <InertialSensor> getSensor( std::string name)
         return NULL;
     }
 }
+
+/**
+ * \brief This function creates the object Sensor 
+ */
+std::string get_sensor_name(int argc, char *argv[])
+{
+    if (get_navio_version() == NAVIO2) {
+
+        if (argc < 2) {
+            printf("Enter parameter\n");
+            return std::string();
+        }
+
+        // prevent the error message
+        opterr = 0;
+        int parameter;
+
+        while ((parameter = getopt(argc, argv, "i:h")) != -1) {
+            switch (parameter) {
+            case 'i': return optarg;
+            case 'h': print_help(); return "-1";
+            case '?': printf("Wrong parameter.\n");
+                      print_help();
+                      return std::string();
+            }
+        }
+
+    } else { //sensor on NAVIO+
+
+        return "mpu";
+    }
+
+/**
+ * \brief This function allows you to read the values of the accelerometer,
+ * the gyroscope and the magnetometer 
+ * \param[out] obj the pointer to the tabs 
+ * \param[in] tabaccel tab of floats with the values of the acceleromter in the 3 directions
+ * \param[in] tabgyr tab of floats with the values of the gyrometer in the 3 directions
+ * \param[in] tabmag tab of floats with the values of the magnetometer in the 3 directions
+ */
+void read(float &tabaccel, float &tabgyr, float &tabmag, auto &obj){
+  obj->read_accelerometer(tabaccel,tabaccel+1,tabaccel+2);
+  obj->read_gyroscope(tabgyr,tabgyr+1,tabgyr+2);
+  obj->read_magnetometer(tabmag,tabmag+1,tabmag+2);
+}
+
+//======================================================================================================
+
+int main(int argc, char *argv[]) {
+
+    if (check_apm()) {
+        return 1;
+    }
+
+    auto sensor_name = get_sensor_name(argc, argv);
+    if (sensor_name.empty())
+        return EXIT_FAILURE;
+
+    auto sensor = get_inertial_sensor(sensor_name);
+
+    if (!sensor) {
+        printf("Wrong sensor name. Select: mpu or lsm\n");
+        return EXIT_FAILURE;
+    }
+
+    if (!sensor->probe()) {
+        printf("Sensor not enabled\n");
+        return EXIT_FAILURE;
+    }
+    sensor->initialize();
+
+    float tableauaccel[3];
+    float tabbleaumag[3];
+    float tableaugyr[3];
+    auto sensor = getSensor(sensor_name);
+
+    while(1){
+        sensor->update();
+        sensor->read(&tableauaccel, &tabelaumag, &tableaugyr, &sensor);
+
+        usleep(500000);
+    }
+}
+
+    
