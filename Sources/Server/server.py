e7a0e31232a21f651f65906fc86bbdc2aab14425
torressantiago <torres_santiago@javeriana.edu.co>
2021-04-25 13:10:31 +0100

    Finalized server.py

--- Sources/Server/server.py
+++ Sources/Server/server.py
@@ -2,6 +2,16 @@
 import socket
 import sys
 
+# Variable initialization
+indexIMU = 0
+indexGPS = 0
+IMUFlag = 0
+GPSFlag = 0
+IMU = []
+IMU_rec = []
+GPS = []
+GPS_rec = []
+
 #create the server socket
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
@@ -16,13 +26,39 @@ print("Server listening")
 #listen to incoming datagrams
 while(True):
     data, addr = server_socket.recvfrom(1024)
-    message = data
-    address = addr
-    clientMsg = "Client msg : {}".format(message)
-    clientAddr = "Client addr : {}".format(address)
-    print(clientMsg)
-    print(clientAddr)
+    message = data.decode("utf-8")
 
-    #send response
-    msg = str.encode("Hello Client!")
-    server_socket.sendto(msg, address)
+    num = message;
+    index = num[0]
+    if index == 'I' and IMUFlag == 0:
+        IMU.append(float(num[1:-1]))
+        indexIMU = indexIMU+1
+        if indexIMU == 8:
+            IMU_rec = IMU
+            IMU = []
+            indexIMU = 0
+            IMUFlag = 1
+        #print("IMU: ",IMU[indexIMU])
+    elif index == 'G' and GPSFlag == 0:
+        GPS.append(float(num[1:-1]))
+        indexGPS = indexGPS+1
+        if indexGPS == 6:
+            GPS_rec = GPS
+            GPS = []
+            indexGPS = 0
+            GPSFlag = 1
+        #print("GPS: ",GPS[indexGPS])
+    elif index == 'T':
+        Temp = float(num[1:-1])
+        #print("Temp: ",Temp)
+    
+    if IMUFlag == 1:
+        IMUFlag = 0
+        for i in range(len(IMU_rec)):
+            print("IMU: ",IMU_rec[i])
+    if GPSFlag == 1:
+        GPSFlag = 0
+        for i in range(len(GPS_rec)):
+            print("GPS: ",GPS_rec[i])
+    print("Temp: ",Temp)
+    
