65001caf58ab3fb5d8b2acfa15de491b00589623
torressantiago <torres_santiago@javeriana.edu.co>
2021-04-26 17:33:02 +0200

    added features to server

--- Sources/Server/server.py
+++ Sources/Server/server.py
@@ -12,6 +12,8 @@ IMU_rec = []
 GPS = []
 GPS_rec = []
 
+
+
 #create the server socket
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
@@ -23,7 +25,7 @@ Port = 8080          # port
 server_socket.bind((Host,Port))
 print("Server listening")
 
-#listen to incoming datagrams
+#listen to incoming datagrams and decode received data
 while(True):
     data, addr = server_socket.recvfrom(1024)
     message = data.decode("utf-8")
@@ -33,7 +35,7 @@ while(True):
     if index == 'I' and IMUFlag == 0:
         IMU.append(float(num[1:-1]))
         indexIMU = indexIMU+1
-        if indexIMU == 8:
+        if indexIMU == 9:
             IMU_rec = IMU
             IMU = []
             indexIMU = 0
@@ -42,7 +44,7 @@ while(True):
     elif index == 'G' and GPSFlag == 0:
         GPS.append(float(num[1:-1]))
         indexGPS = indexGPS+1
-        if indexGPS == 6:
+        if indexGPS == 7:
             GPS_rec = GPS
             GPS = []
             indexGPS = 0
@@ -52,13 +54,39 @@ while(True):
         Temp = float(num[1:-1])
         #print("Temp: ",Temp)
     
+
+    # Record received data
     if IMUFlag == 1:
         IMUFlag = 0
-        for i in range(len(IMU_rec)):
-            print("IMU: ",IMU_rec[i])
+        accx = float(IMU_rec[0]) # m/s^2
+        accy = float(IMU_rec[1])
+        accz = float(IMU_rec[2])
+        
+        gyrx = float(IMU_rec[3]) # 
+        gyry = float(IMU_rec[4])
+        gyrz = float(IMU_rec[5])
+        
+        magx = float(IMU_rec[6]) #
+        magy = float(IMU_rec[7])
+        magz = IMU_rec[8]
+
+        print("------Accelerometer------")
+        print("ax = ",accx," ay = accy = ", accy, " accz = ", accz, "\n")
+        #for i in range(len(IMU_rec)):
+        #    print("IMU: ",IMU_rec[i])
     if GPSFlag == 1:
         GPSFlag = 0
-        for i in range(len(GPS_rec)):
-            print("GPS: ",GPS_rec[i])
-    print("Temp: ",Temp)
-    
+        GPS_mill_T_week = float(GPS_rec[0])/1000 # GPS Millisecond time of the week (s)
+        Longitude = float(GPS_rec[1])/10000000
+        Latitude = float(GPS_rec[2])/10000000
+        H_above_ell = float(GPS_rec[3])/1000 # Height above Ellipsoid (m)
+        H_above_sea = float(GPS_rec[4])/1000 # Height above sea level (m)
+        Hor_acc_est = float(GPS_rec[5])/1000 # Horizontal accuracy estimate (m)
+        Ver_acc_est = float(GPS_rec[6])/1000 # Vertical accuracy estimate (m)
+        #for i in range(len(GPS_rec)):
+        #    print("GPS: ",GPS_rec[i])
+        print("------GPS------")
+        print("Latitude: ", Latitude, " Longitude: ", Longitude, "\n")
+        print("------Temperature------")
+        print(Temp,"\n")
+    # print("Temp: ",Temp)
